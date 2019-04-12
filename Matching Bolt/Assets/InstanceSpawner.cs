e9d5667be15f94cf1d87dd36a12cfc2760e115b5
Daniel Michaneck <daniel.michaneck.5180@student.uu.se>
2019-04-12 17:21:37 +0200

    Added Projectiles and NPC Spawning

---
+++ Matching Bolt/Assets/InstanceSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceSpawner : MonoBehaviour
+{
+    private GameObject instancePlane;
+    private float spawnTimer;
+
+    public GameObject instanceObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instancePlane = transform.parent.Find("Game Plane").Find("Instance Plane").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer > 0)
+        {
+            spawnTimer -= Time.deltaTime;
+        }
+        else
+        {
+            spawnTimer = 1f;
+
+            GameObject instance = Instantiate(instanceObject, instancePlane.transform);
+            instance.transform.position = new Vector3(-40, instance.transform.position.y, instance.transform.position.z + Random.Range(-20, 20));
+            instance.GetComponent<Rigidbody>().velocity = new Vector3(10, 0, 0);
+        }
+    }
+}
