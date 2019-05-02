ed3517519c608cc5dfae544203dc0899af365ab0
Jesper <Jesper.Pettersson.2310@student.uu.se>
2019-05-02 17:07:37 +0200

    Update PlayerCamera.cs

--- Matching Bolt/Assets/Scripts/Player/PlayerCamera.cs
+++ Matching Bolt/Assets/Scripts/Player/PlayerCamera.cs
@@ -19,16 +19,14 @@ public class PlayerCamera : MonoBehaviour
     
     void Update()
     {
-        /*
         playerCamera.transform.position = startPosition;
         float[] infra = new float[3];
         for (int i = 0; i < 3; i++)
         {
             infra[i] = GetComponent<WiimoteScript>().GetWiimotePosition()[i];
         }
-        infra[0] -= 0.5f; infra[1] -= 0.5f; infra[2] -= 0.5f;
-        Vector3 newVector = new Vector3(infra[0] * invertedHorizontal, infra[1] * invertedVertical * -1, infra[2] / 2000);
+        infra[0] -= 0.5f; infra[1] -= 0.5f;// infra[2] -= 0.5f;
+        Vector3 newVector = new Vector3(infra[0] * invertedHorizontal, infra[1] * invertedVertical * -1, infra[2] / 1500);
         playerCamera.transform.Translate(newVector * cameraSensitivity, Space.Self);
-        */
     }
 }
