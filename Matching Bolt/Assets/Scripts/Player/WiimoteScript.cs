1657cff284c03b3d1e20f40a922b46a0dd5c8d5d
Jesper <Jesper.Pettersson.2310@student.uu.se>
2019-05-07 17:50:12 +0200

    add support for 2 wiimote WIP

--- Matching Bolt/Assets/Scripts/Player/WiimoteScript.cs
+++ Matching Bolt/Assets/Scripts/Player/WiimoteScript.cs
@@ -13,44 +13,74 @@ public class WiimoteScript : MonoBehaviour
     private float ir2;
     private bool bHeld;
     private bool bDown;
-    public int CamerWiimote;
-    public int GunWiimote;
+    public int Camera;
+    public int Crossbow;
+    private int cameraWiimote;
+    private int crossWiimote;
+
 
     private void Awake()
     {
         WiimoteManager.FindWiimotes();
-        wiimote1 = WiimoteManager.Wiimotes[CamerWiimote];
-        wiimote2 = WiimoteManager.Wiimotes[GunWiimote];
+        wiimote1 = WiimoteManager.Wiimotes[Camera];
+        wiimote2 = WiimoteManager.Wiimotes[Crossbow];
         wiimote1.SetupIRCamera(IRDataType.BASIC);
         wiimote2.SetupIRCamera(IRDataType.BASIC);
         ir = new float[3];
+        cameraWiimote = 0;
+        crossWiimote = 1;
     }
 
     void Update()
+    {
+        Updatewiimote(wiimote1);
+        Updatewiimote(wiimote2);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SwtichWiimotes();
+        }
+
+    }
+
+    public void SwtichWiimotes()
+    {
+        if(cameraWiimote == 0)
+        {
+            cameraWiimote = 1;
+            crossWiimote = 0;
+        }
+        else
+        {
+            cameraWiimote = 0;
+            crossWiimote = 1;
+        }
+    }
+
+    private void Updatewiimote( Wiimote wiimote)
     {
         wiimote.ReadWiimoteData();
 
         float[] tempir = wiimote.Ir.GetPointingPosition();
         ir0 = tempir[0];
         ir1 = tempir[1];
-        
+
         float[,] sensorIR = wiimote.Ir.GetProbableSensorBarIR(true);
         float xDis = sensorIR[0, 0] - sensorIR[0, 1];
         float yDis = sensorIR[1, 0] - sensorIR[1, 1];
         ir2 = Vector2.Distance(new Vector2(sensorIR[0, 0], sensorIR[0, 1]), new Vector2(sensorIR[1, 0], sensorIR[1, 1]));
-        
+
         Debug.Log("IR0: " + ir0.ToString());
         Debug.Log("IR[0]: " + ir[0].ToString());
         Debug.Log("IR1: " + ir1.ToString());
         Debug.Log("IR[1]: " + ir[1].ToString());
         Debug.Log("IR2: " + ir2.ToString());
         Debug.Log("IR[2]: " + ir[2].ToString());
-        
+
         if (ir0 > 0)
         {
             ir[0] = ir0;
         }
-        
+
         if (ir1 > 0)
         {
             ir[1] = ir1;
@@ -69,7 +99,7 @@ public class WiimoteScript : MonoBehaviour
         {
             bDown = true;
         }
-        
+
         if (wiimote.Button.b == true)
         {
             bHeld = true;
