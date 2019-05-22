444190c431109ba3572a5715e1a0df562bbde263
Daniel Michaneck <daniel.michaneck@gmail.com>
2019-05-22 15:20:50 +0200

    Merge Fixed

---
+++ Matching Bolt/Assets/Scripts/Controller/GameHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+    private float difficultyMultiplier;
+    private bool gameRunning;
+
+    void Awake()
+    {
+        gameRunning = true;
+        Time.timeScale = 1;
+        difficultyMultiplier = 1;
+    }
+
+    private void Update()
+    {
+        UpdateDifficulty();
+
+        if (Input.GetKeyDown(KeyCode.F) == true)
+        {
+            EndGame();
+        }
+        if (Input.GetKeyDown(KeyCode.R) == true)
+        {
+            SceneManager.LoadScene("PERSPECTIVE Daniel's Scene");
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            GetComponent<TextHandler>().DisplayHighScore();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+            }
+            else
+            {
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    private void UpdateDifficulty()
+    {
+        difficultyMultiplier += Time.deltaTime / 60;
+    }
+
+    public float GetDifficultyMultiplier()
+    {
+        return difficultyMultiplier;
+    }
+
+    public void EndGame()
+    {
+        gameRunning = false;
+        GetComponent<TextHandler>().DisplayHighScore();
+        Application.Quit();
+    }
+}
