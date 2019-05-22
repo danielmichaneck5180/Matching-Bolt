92a2405e033fd153558f183d5f40c560b6aa74fa
Daniel Michaneck <daniel.michaneck@gmail.com>
2019-05-22 15:56:41 +0200

    Fix Attempt Fixed?

---
+++ Matching Bolt/Assets/Scripts/Obstacle/ObstacleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    public int obstacleSize;
+    private GameObject sprite;
+    private GameObject player;
+
+    private void Awake()
+    {
+        sprite = transform.Find("Rotation").gameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        RotateToCamera();
+    }
+
+    public int GetObstacleSize()
+    {
+        return obstacleSize;
+    }
+
+    private void RotateToCamera()
+    {
+        sprite.transform.rotation = Quaternion.Euler(Vector3.RotateTowards(sprite.transform.rotation.eulerAngles, player.transform.Find("Main Camera").transform.rotation.eulerAngles, 10000f, 1000f));
+        sprite.transform.rotation = Quaternion.Euler(new Vector3(sprite.transform.rotation.eulerAngles.x, 0, sprite.transform.rotation.eulerAngles.z));
+    }
+}
---
+++ Matching Bolt/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float cameraSensitivity;
+    public int invertedHorizontal;
+    public int invertedVertical;
+    public bool wiimoteEnabled;
+
+    private Vector3 startPosition;
+    private GameObject camera;
+    private float sideDistance;
+
+    void Awake()
+    {
+        camera = transform.Find("Main Camera").gameObject;
+        startPosition = camera.transform.position;
+        sideDistance = 10f;
+    }
+    
+    void Update()
+    {
+        if (wiimoteEnabled == true)
+        {
+            camera.transform.position = startPosition;
+            float[] infra = new float[3];
+            for (int i = 0; i < 3; i++)
+            {
+                infra[i] = GetComponent<WiimoteScript>().GetCameraWiimotePosition()[i];
+            }
+            infra[0] -= 0.5f; infra[1] -= 0.5f;// infra[2] -= 0.5f;
+            Vector3 newVector = new Vector3(infra[0] * invertedHorizontal, infra[1] * invertedVertical * -1, infra[2] / 1500);
+            camera.transform.Translate(newVector * cameraSensitivity, Space.Self);
+        }
+        else
+        {
+            int xDir = CalcDir(KeyCode.A, KeyCode.D, camera.transform.position.x, startPosition.x);
+            int yDir = CalcDir(KeyCode.S, KeyCode.W, camera.transform.position.z, startPosition.z);
+
+            camera.transform.position = new Vector3(camera.transform.position.x + (xDir * sideDistance * Time.deltaTime * 2), camera.transform.position.y, camera.transform.position.z + (yDir * sideDistance * Time.deltaTime * 2));
+
+            if (Mathf.Abs(camera.transform.position.x) > Mathf.Abs(startPosition.x + (xDir * sideDistance)))
+            {
+                camera.transform.position = new Vector3(startPosition.x + (xDir * sideDistance), camera.transform.position.y, camera.transform.position.z);
+            }
+
+            if (Mathf.Abs(camera.transform.position.z) > Mathf.Abs(startPosition.z + (yDir * sideDistance)))
+            {
+                camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, startPosition.z + (yDir * sideDistance));
+            }
+        }
+    }
+
+    private int CalcDir(KeyCode A, KeyCode B, float a, float b)
+    {
+        int dir = 0;
+        if (Input.GetKey(A) == true)
+        {
+            dir -= 1;
+        }
+        if (Input.GetKey(B) == true)
+        {
+            dir += 1;
+        }
+        if (dir == 0)
+        {
+            float dist = a - b;
+            if (dist > 1f)
+            {
+                if (dist - 1 < 0)
+                {
+                    dir = Mathf.RoundToInt(-dist);
+                }
+                else
+                {
+                    dir = -1;
+                }
+            }
+            else if (dist < -1f)
+            {
+                if (dist + 1 > 0)
+                {
+                    dir = Mathf.RoundToInt(dist);
+                }
+                else
+                {
+                    dir = 1;
+                }
+            }
+        }
+        return dir;
+    }
+}
