93930ddf8b339656acb348b348ab1363fb972975
Daniel Michaneck <daniel.michaneck@gmail.com>
2019-05-22 15:32:26 +0200

    Merge Fix Attempt 2 Finish

---
+++ Matching Bolt/Assets/Scripts/Controller/ObstacleScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    public int obstacleSize;
+
+    public int GetObstacleSize()
+    {
+        return obstacleSize;
+    }
+}
